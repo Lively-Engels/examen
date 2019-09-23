159a4667b00eeb6f324b1d06a4dfb1d73d0729d6
LEONEL PEDRO ANGELES ESPARZA <liedstein@gmail.com>
2019-09-23 11:29:29 -0500

    examen

---
+++ piramide.py
@@ -0,0 +1,14 @@
+def triangulo (base):
+    
+   num1=0
+   num2=0
+    
+   for c in range (base):
+        num1=0
+        while(num1<=num2):
+            print("*", end="")
+            num2 = num2 +1
+   return (base)         
+x=triangulo(9)
+print(x)
+                          
